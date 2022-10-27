c3965220c0ca72ee3336c63cf84729906c0d73e5
Alicia Glenn <alicia@aliciaglenn.com>
2022-10-26 20:03:31 -0500

    add the promise to a button so that when clicked data is rendered to the screen

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-$(function() {
-
+// $(function () {
 const promise = $.ajax({
-//  url:'https://the-sneaker-database.p.rapidapi.com/sneakers'
-// method: 'GET',
-url: 'https://the-sneaker-database.p.rapidapi.com/sneakers?limit=20',
-// params: {limit: 20},
-headers: {
-'X-RapidAPI-Key': '8e228d85bemsh56c6591ce73d9c1p18f21cjsn854021b29e31',
-'X-RapidAPI-Host': 'the-sneaker-database.p.rapidapi.com'
-
-}
-
+  //  url:'https://the-sneaker-database.p.rapidapi.com/sneakers'
+  // method: 'GET',
+  url: "https://the-sneaker-database.p.rapidapi.com/sneakers?limit=20",
+  // params: {limit: 20},
+  headers: {
+    "X-RapidAPI-Key": "8e228d85bemsh56c6591ce73d9c1p18f21cjsn854021b29e31",
+    "X-RapidAPI-Host": "the-sneaker-database.p.rapidapi.com",
+  },
 });
 
-
+// create a text input box (string interpolation)
+// below that add the click button that we created
+// user types in the number of sneakers he /she wants returned and after they will click on the button and it will return the number of shoes they specifc
 
 // const options = {
 //     method: 'GET',
@@ -25,37 +24,29 @@ headers: {
 //     }
 //   };
 
+// put inside of function. Have button on click call  the function
 
-
-
-promise.then(
+const hello = () => {
+  console.log("hello");
+  // clear out list of 20 then add to it
+  promise.then(
     (data) => {
-        console.log(data)
-        // render(data.results)
-        return data
+      console.log(data.results);
+
+      data.results.forEach((sneaker) => {
+        const $h1 = $(`<h1>${sneaker.name}</h1>`);
+        $("body").append($h1);
+      });
+
+      // render(data.results)
+      return data;
     },
 
     (error) => {
-        console.error(error)
-        // console.log(error)
+      console.error(error);
+      // console.log(error)
     }
-)
-})
+  );
+};
 
-//render function to the screen using a function 
-
-function render(sneakers){
-    sneakers.forEach((sneaker) => {
-const sneakerData = getsneakerData(sneaker)
-console.log(sneakerData)
-const $h1 = $(`<h1>${sneakers.name}</h1>`)
-$('body').append($h1)
-})
-}
-
-function sneakerData(sneaker){
-    console.log(sneaker)
-    const promise = $.ajax({
-        url:sneaker.url
-    })
-}
+$("button").click(hello);
